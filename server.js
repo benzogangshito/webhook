26cbff1f94ad91f3703169f8e2996c32858a5aad
Benzogangshito <72577444+benzogangshito@users.noreply.github.com>
2026-05-22 22:22:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const BOT_TOKEN = process.env.BOT_TOKEN || "";
 const PORT = process.env.PORT || 3000;
-const BOT_API_BASE = "https://short-files-listen.loca.lt";
+const BOT_API_BASE = "https://warm-doors-cheat.loca.lt";
 
 const orders = [];
 
