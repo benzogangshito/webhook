d1c4d69bb26e9e218ed163db5feff0414b854774
Benzogangshito <72577444+benzogangshito@users.noreply.github.com>
2026-05-25 00:41:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const BOT_TOKEN = process.env.BOT_TOKEN || "";
 const PORT = process.env.PORT || 3000;
-const BOT_API_BASE = "https://kind-pants-divide.loca.lt";
+const BOT_API_BASE = "https://smart-cloths-tap.loca.lt";
 
 const orders = [];
 
