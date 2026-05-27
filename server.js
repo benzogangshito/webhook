5166cbde66cf849a2182cdbeaf2f7d7db97950ae
Benzogangshito <72577444+benzogangshito@users.noreply.github.com>
2026-05-27 12:43:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const BOT_TOKEN = process.env.BOT_TOKEN || "";
 const PORT = process.env.PORT || 3000;
-const BOT_API_BASE = "https://chilly-zoos-add.loca.lt";
+const BOT_API_BASE = "https://legal-needles-dance.loca.lt";
 
 const orders = [];
 
